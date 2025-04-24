dafb32a5e01d6864fbb002f5d44b2a1b0a6bad3d
Sarvinoztashbaeva <tashbaevasarvinoz@gmail.com>
2025-04-24 17:29:47 +0500

    Create strings.py

---
+++ lesson-1/homework/strings.py
@@ -0,0 +1,22 @@
+a = 7
+P = 4*a
+A = a**2
+print(P, A)
+
+d = 5
+pi = 3.14
+L = d*pi
+print(L)
+
+a = 8
+b = 4
+mean = (a+b)/2
+print(mean)
+
+a = 11
+b = 7
+sum = a+b
+product = a*b
+square_a = pow(a,2)
+square_b = pow(b,2)
+print(square_a, square_b)
